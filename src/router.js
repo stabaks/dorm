ac66839063a824d6d1b158de419615bd98a78234
Li Linyang <1264576767@qq.com>
2019-08-08 17:43:23 +0800

    3

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from './views/Home'
+import login from './views/Login'
 
 Vue.use(Router)
 
@@ -8,13 +9,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: home
+      name: 'login',
+      component: login
     },{
       path: '/home',
       name: 'home',
       component: home
-    },
+    },{
+      path: '/login',
+      name: 'login',
+      component: login
+    }
     
   ]
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,46 @@
+<!--  -->
+<template>
+  <div class="wrapper">
+      <div class="login-wrapper">
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {}
+}
+
+</script>
+<style scoped>
+    .wrapper {
+        background-image: url('../assets/login-back.png');
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1000;
+        display: flex;
+        align-items: center
+    }
+    .login-wrapper{
+        position: absolute;
+        width: 33.3%;
+        height: 45%;
+        left: 15%;
+        background-color: rgb(189, 118, 118)
+    }
+</style>
